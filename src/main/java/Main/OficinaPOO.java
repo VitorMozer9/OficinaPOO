de848ed023cb2d81c1f8bf9be4ec407303444aca
Vitor Mozer <vectors3whittzz@gmail.com>
2025-04-27 14:04:40 -0300

    Criando metodo de validação de placa do carro

--- src/main/java/Main/OficinaPOO.java
+++ src/main/java/Main/OficinaPOO.java
@@ -2,6 +2,7 @@ package Main;
 
 import Model.Cpf;
 import Model.Pessoa;
+import Model.Veiculo;
 
 public class OficinaPOO {
 
@@ -9,8 +10,13 @@ public class OficinaPOO {
         String cpfAvulso = "906.884.200-52";
         Cpf cpfPessoa = new Cpf(cpfAvulso);
         
+        Veiculo carro = new Veiculo("Modelo", "HEE-7480", "Manutenção", 2005);
+        
         System.out.println("Cpf sem anonimização " + cpfPessoa.getNumeroCpf());
         System.out.println("Cpf anonimizado " + cpfPessoa.toString());
+        
+        System.out.println("Placa do veiculo: " + carro.getPlacaVeiculo());
+        System.out.println("A placa é valida? " + Veiculo.validaPlaca(carro.getPlacaVeiculo()));
         
         //System.out.println("Retorno :" + Cpf.validaCPF("906.884.200-52"));
     }
--- src/main/java/Model/Veiculo.java
+++ src/main/java/Model/Veiculo.java
@@ -12,12 +12,52 @@ public class Veiculo {
         this.modeloVeiculo = modeloVeiculo;
         this.placaVeiculo = placaVeiculo;
         this.anoDeFabricacao = anoDeFabricacao;
+    }
+    
+    public String getModeloVeiculo(){
+        return modeloVeiculo;
+    }
+    
+    public void setModeloVeiculo(String modeloVeiculo){
+        this.modeloVeiculo = modeloVeiculo;
+    }
+    
+    public String getPlacaVeiculo(){
+        return placaVeiculo;
+    }
+    
+    public void setPlacaVeiculo(String placaVeiculo){
+        this.placaVeiculo = placaVeiculo;
+    }
+    
+    public String getStatusVeiculo(){
+        return statusVeiculo;
+    }
+    
+    public void setStatstusVeiculo(String statusVeiculo){
+        this.statusVeiculo = statusVeiculo;
+    }
+    
+    public int getAnoDeFabricacao(){
+        return anoDeFabricacao;
+    }
+    
+    public void setAnoDeFabricacao(int anoDeFabricacao){
+        this.anoDeFabricacao = anoDeFabricacao;
+    }
+    
+    public static boolean validaPlaca(String placaVeiculo){
+        String placa = placaVeiculo.replaceAll("-", "").trim().toUpperCase();
         
-//    public satatic boolean ValidaPlaca() {
-//        String placa = placaVeiculo.trim().toUpperCase();
-//        return placa.matches("^[A-Z]{3}[0-9][A-Z][0-9]{2}$");
-//    }
+        if (placaVeiculo.isEmpty()) {
+            return false;
+        }
+    
+        boolean padraoMercosul = placa.matches("^[A-Z]{3}[0-9][A-Z][0-9]{2}$");
+        boolean padraoAntigo = placa.matches("^[A-Z]{3}[0-9]{4}$");
         
+        return padraoMercosul || padraoAntigo;
+    
     }
     
 }
