ade0927043686973c4ec66c535913e254f1ccf34
Vitor Mozer <vectors3whittzz@gmail.com>
2025-05-03 14:17:18 -0300

    Criando metodo de adicionar cliente e salvar dados do cliente

--- src/main/java/View/ClienteView.java
+++ src/main/java/View/ClienteView.java
@@ -5,29 +5,57 @@ import java.util.Scanner;
 public class ClienteView {
     Scanner leituraDados = new Scanner(System.in);
     
-    public int exibirMenu(){
-        int opcao = 0;
-        
-        while (opcao != 5){
-            System.out.println("1 - Incluir Cliente (Fazer Reserva)");
-            System.out.println("2 - Editar Cliente");
-            System.out.println("3 - Remover Cliente");
-            System.out.println("4 - Mostrar dados de um Cliente");
-            System.out.println("5 - Sair");
-            System.out.println();
-            
-            System.out.println("Digite a opcao: ");
-            opcao = leituraDados.nextInt();
-            leituraDados.nextLine();
-            return opcao;
-            
-        }
-        return 4;
+    public int mostraOpcoesCliente(){
+        System.out.println("Digite a opção que deseja exucutar: \n");
+        System.out.println("1 - Incluir Cliente");
+        System.out.println("2 - Editar Cliente");
+        System.out.println("3 - Remover Cliente");
+        System.out.println("4 - Mostrar dados de um Cliente");
+        System.out.println("5 - Sair");
+        System.out.println();
+
+        int opcao = leituraDados.nextInt();
+        leituraDados.nextLine();
+        return opcao;
     }
     
     public String getNomeCliente(){
         System.out.println("Digite o nome do cliente: ");
         return leituraDados.nextLine();
         }
+    
+    public String getEnderecoCliente(){
+        System.out.println("Digite o endereço do cliente: ");
+        return leituraDados.nextLine();
+    }
+    
+    public String getFoneCliente(){
+        System.out.println("Digite o telefone do cliente: ");
+        return leituraDados.nextLine();
+    }
+    
+    public String getEmailCliente(){
+        System.out.println("Digite o email do cliente: ");
+        return leituraDados.nextLine();
+    }
+    
+    public String getCpfCliente(){
+        System.out.println("Digite o CPF do cliente: ");
+        return leituraDados.nextLine();
+    }
+    
+    public int editaCliente(){
+        System.out.println("Digite o campo que você gostaria de editar!");
+        System.out.println("1 - Endereço");
+        System.out.println("2 - Telefone");
+        System.out.println("3 - E-mail");
+      
+        int opcao = leituraDados.nextInt();
+        leituraDados.nextLine();
+        return opcao;
+    }
+
+    
+    
     
 }
--- src/main/java/controller/ClienteController.java
+++ src/main/java/controller/ClienteController.java
@@ -1,6 +1,65 @@
 package controller;
 
-public class ClienteController {
+import Model.Cliente;
+import Model.Cpf;
+import View.ClienteView;
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class ClienteController {
+    private ClienteView viewCliente = new ClienteView();
+    private List<Cliente> listaClientes = new ArrayList<>();
+    private final String salvaClientes = "clientes.json"; 
+    
+    public void adicionaCliente(){
+        String nome = viewCliente.getNomeCliente();
+        String endereco = viewCliente.getEnderecoCliente();
+        String telefone = viewCliente.getFoneCliente();
+        String email = viewCliente.getEmailCliente();
+        String codigoCpf = viewCliente.getCpfCliente();
+
+        Cpf cpf = new Cpf(codigoCpf); 
+
+        Cliente novoCliente = new Cliente(nome, endereco, telefone, email, cpf);
+        listaClientes.add(novoCliente);
+        salvarClientes();
+        System.out.println("Cliente adicionado com sucesso!");
+         
+    }
+    
+     private void salvarClientes() {
+        Gson gson = new Gson();
+        try (FileWriter writer = new FileWriter(salvaClientes)) {
+            gson.toJson(listaClientes, writer);
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar clientes: " + e.getMessage());
+        }
+    }
+     
+    public void executaMenuCliente(){
+        int opcao = 0; 
+        
+        while(opcao != 5){
+            opcao = viewCliente.mostraOpcoesCliente();
+            
+            switch (opcao){
+                case 1 -> {
+                    adicionaCliente();
+                }
+//                case 2 -> {
+//                    editaCliente();
+//                }
+//                case 3 -> {
+//                    removeCliente();
+//                }
+//                case 4 -> {
+//                    mostraCliente();
+                }
+            }
+        }
 }
+
  
